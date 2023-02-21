86bc5ecd0ca5a081337fd2ca09e07d118ae5d761
Tania <tatsianashelest@gmail.com>
2023-02-21 18:48:39 +0300

    Задача1

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("n = ");
+        var n = Convert.ToInt32(Console.ReadLine());
+        var sum = 0;
+        while(n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        Console.WriteLine(sum);
+    }
+}
